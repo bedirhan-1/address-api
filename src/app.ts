ead43fe21ce096fb2fa9c496420accbb6a7b7e66
İsa Zobu <bedirhan9828@gmail.com>
2024-05-22 20:06:22 +0300

    feat: add Addresses route

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
-import express from 'express';
+import express, { Router } from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv'
 import connectDB from './config/database';
+const cors = require('cors') // it doesnt support es6 
+import * as addressController from './controller/AddressController';
+
 
 dotenv.config();
 
@@ -14,9 +17,22 @@ const PORT = process.env.PORT || 3000;
 connectDB();
 
 
-// BodyParser setup
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors())
+
+
+const router = Router();
+
+router.post('/addresses', addressController.createAddress);
+router.get('/addresses', addressController.getAllAddresses);
+router.get('/addresses/:id', addressController.getAddress);
+router.put('/addresses/:id', addressController.updateAddress);
+router.delete('/addresses/:id', addressController.deleteAddress);
+
+
+app.use('/api/v1', router);
+
 
 
 app.listen(PORT, () => console.log(`Your server is running on port ${PORT}`));
