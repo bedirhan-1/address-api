89e8580918c1ea4ef84ec713a71abe0204b73253
İsa Zobu <bedirhan9828@gmail.com>
2024-05-22 20:05:54 +0300

    feat: add Address  modal

---
+++ src/model/address.ts
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+export interface IAddress extends mongoose.Document {
+    city: string,
+    addressTitle: string,
+    addressDetails: string,
+    _id: string
+}
+
+export type CreateAddressDto = {
+    city: string,
+    addressTitle: string,
+    addressDetails: string
+}
+
+
+const addressSchema = new mongoose.Schema({
+    city: { type: String, required: true },
+    addressTitle: { type: String, required: true },
+    addressDetails: { type: String, required: true },
+},{
+
+    
+} );
+
+
+
+
+const Address = mongoose.model<IAddress>('Address', addressSchema);
+
+export default Address;
